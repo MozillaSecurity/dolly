7aecddcaf3507a5ca12e4be3ace346050dc69d37
Christoph Diehl <cdiehl@mozilla.com>
2017-05-16 22:41:23 +0200

    Update classifiers in setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -56,6 +56,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements
